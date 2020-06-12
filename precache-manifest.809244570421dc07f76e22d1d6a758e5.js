ff59f7e4471c007e29ae026785d56e98f43e5969
Tejas Bavaskar <44202599+TejasBavaskar@users.noreply.github.com>
2020-06-12 19:10:51 +0530

    Updates

---
+++ precache-manifest.809244570421dc07f76e22d1d6a758e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367e423aa5cf5e6899df66e459399908",
+    "url": "/deploy_react/index.html"
+  },
+  {
+    "revision": "c7c574b631b4abe29f13",
+    "url": "/deploy_react/static/css/main.89803a33.chunk.css"
+  },
+  {
+    "revision": "4fef870b4debb4a991b5",
+    "url": "/deploy_react/static/js/2.1dba1c6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy_react/static/js/2.1dba1c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7c574b631b4abe29f13",
+    "url": "/deploy_react/static/js/main.d6393de4.chunk.js"
+  },
+  {
+    "revision": "3134e2faccb35d533291",
+    "url": "/deploy_react/static/js/runtime-main.ce24af30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
